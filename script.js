50f33459536a4e842a2834d2aae2ced0d123e82e
Abdubosit01 <abdubosit0115@gmail.com>
2025-10-06 15:10:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+let h1 = document.querySelector('h1')
+
+
+
+new  Typed (h1, {
+    strings: ['TEXT GRADIENTS'],
+    typeSpeed: 500,
+    baskSpeed: 1000,
+    startDaelay:2000,
+    backDaelay:1000,
+    loop:true
+})
+
+
+let p = document.querySelector('p')
+
+
+
+new  Typed (p, {
+    strings: ['Make your headlines stand out'],
+    typeSpeed: 200,
+    baskSpeed: 1000,
+    startDaelay:2000,
+    backDaelay:1000,
+    loop:true
+})
+
+
+
+
+
+
+
+
+
